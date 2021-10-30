ff11c7b77dbfc19443fd768c071a8b233b1381cf
mohamedelkony <konykony22@gmail.com>
2021-10-30 17:23:36 +0200

    Create Candy_Box_D.java

---
+++ Candy_Box_D.java
@@ -0,0 +1,168 @@
+import java.util.*;
+import java.io.*;
+
+/*
+https://codeforces.com/contest/1183/problem/D
+*/
+public class Candy_Box_D {
+    public static void main(String[] str) {
+        boolean muiltTestCases = true;
+        var cin = new FastScanner();
+        PrintWriter cout = new PrintWriter(new BufferedOutputStream(System.out));
+        int n = 1;
+        if (muiltTestCases)
+            n = cin.nextInt();
+        var s = new testCaseSolution(cin, cout);
+        while (n-- > 0)
+            s.solve();
+        cout.flush();
+    }
+}
+
+class testCaseSolution {
+    private int[][] mem;
+    private FastScanner cin;
+    PrintWriter cout;
+
+    public testCaseSolution(FastScanner cin, PrintWriter cout) {
+        this.cin = cin;
+        this.cout = cout;
+    }
+
+    void solve() {
+        int n = cin.nextInt();
+        var frq = new TreeMap<Integer, Integer>();
+        for (int i = 0; i < n; i++) {
+            int x = cin.nextInt();
+            frq.putIfAbsent(x, 0);
+            frq.put(x, frq.get(x) + 1);
+        }
+        int mx = 0;
+        var acc = new TreeMap<Integer, Integer>(Collections.reverseOrder());
+        for (var e : frq.entrySet()) {
+            int v = e.getValue();
+            acc.putIfAbsent(v, 0);
+            acc.put(v, acc.get(v) + 1);
+        }
+        mx = acc.firstKey();
+        long ans = 0;
+        for (var e : acc.entrySet()) {
+            if (mx == 0)
+                break;
+            if (e.getKey() < mx)
+                mx = e.getKey();
+            for (int i = 0; i < e.getValue(); i++) {
+                ans += mx;
+                mx--;
+                if (mx == 0)
+                break;
+           
+            }
+        }
+        cout.println(ans);
+    }
+}
+
+final class Algo {
+    private Algo() {
+    }
+
+    // O(log(n))
+    public static long gcd(long a, long b) {
+        if (b == 0)
+            return a;
+        return gcd(b, a % b);
+    }
+
+    // O(log(sqrt(n))
+    public static TreeSet<Integer> getDivisors(int n) {
+        var st = new TreeSet<Integer>();
+        for (int i = 1; i * i <= n; i++) {
+            if (n % i == 0)
+                st.add(i);
+            if (n % (n / i) == 0)
+                st.add(n / i);
+        }
+        return st;
+    }
+
+    // O(n(log(log(N)))
+    public static ArrayList<Integer> primesTill_N(int n) {
+        var ans = new ArrayList<Integer>();
+        var prime = new ArrayList<Integer>(n + 1);
+        for (int i = 0; i < n + 1; i++)
+            prime.add(1);
+
+        for (int p = 2; p * p <= n; p++) {
+            if (prime.get(p) == 1) {
+                for (int i = p * p; i <= n; i += p)
+                    prime.set(i, 0);
+            }
+        }
+
+        for (int p = 2; p <= n; p++)
+            if (prime.get(p) == 1)
+                ans.add(p);
+        return ans;
+    }
+
+    // O(sqrt(N))
+    public static TreeMap<Integer, Integer> getPrimeFactors(int n) {
+        var ans = new TreeMap<Integer, Integer>();
+        var primes = primesTill_N((int) (Math.sqrt(n) + 1));
+        for (int p : primes) {
+            while (n % p == 0) {
+                ans.putIfAbsent(p, 0);
+                ans.put(p, ans.get(p) + 1);
+                n /= p;
+            }
+        }
+        if (n > 2) {
+            ans.putIfAbsent(n, 0);
+            ans.put(n, ans.get(n) + 1);
+        }
+        return ans;
+    }
+}
+
+class FastScanner {
+    BufferedReader br;
+    StringTokenizer st;
+
+    public FastScanner() {
+        br = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    String next() {
+        while (st == null || !st.hasMoreElements()) {
+            try {
+                st = new StringTokenizer(br.readLine());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return st.nextToken();
+    }
+
+    int nextInt() {
+        return Integer.parseInt(next());
+    }
+
+    long nextLong() {
+        return Long.parseLong(next());
+    }
+
+    double nextDouble() {
+        return Double.parseDouble(next());
+    }
+
+    String nextLine() {
+        String str = "";
+        try {
+            str = br.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return str;
+    }
+}
