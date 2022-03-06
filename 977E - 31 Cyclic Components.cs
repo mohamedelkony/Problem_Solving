61c390d0e4bf1f90f3f705e448f629badfee1bb4
mohamedelkony <konykony22@gmail.com>
2022-03-06 19:22:14 +0200

    Create 977E - 31 Cyclic Components.cs

---
+++ 977E - 31 Cyclic Components.cs
@@ -0,0 +1,217 @@
+/*
+https://codeforces.com/problemset/problem/977/E
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+public class Solver
+{
+    static int n;
+    static int m;
+    static List<int> state;
+    static List<List<int>> g;
+    static async void Solve()
+    {
+        n = ReadInt();
+        m = ReadInt();
+        g = new List<List<int>>();
+        state = new List<int>();
+        for (int i = 0; i < n; i++)
+        {
+            g.Add(new List<int>());
+            state.Add(0);
+        }
+        for (int i = 0; i < m; i++)
+        {
+            int a = ReadInt();
+            int b = ReadInt();
+            a--; b--;
+            g[a].Add(b);
+            g[b].Add(a);
+        }
+        long ans = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (state[i] == 0)
+                ans += dfs(i, -1)==2?1:0;
+        }
+        Console.WriteLine(ans);
+    }
+    static int dfs(int root, int parent)
+    {
+        state[root] = 1;
+        int valid = 1;
+        for (int i = 0; i < g[root].Count; i++)
+        {
+            int adj=g[root][i];
+            if (adj == parent) continue;
+
+            if (state[adj] != 0)
+            {
+                if (state[adj] == 1 && g[adj].Count == 2)
+                    valid = 2;
+                continue;
+            }
+            valid = dfs(adj, root);
+            if (g[adj].Count > 2)
+                valid = 0;
+
+        }
+        state[root] = 2;
+        if (g[root].Count == 1)
+            return 0;
+
+        return valid;
+    }
+    static string getS(int id)
+    {
+        id++;
+        StringBuilder s = new StringBuilder();
+        while (id > 0)
+        {
+            int d = id % 10;
+            s.Append((char)(d + 'a'));
+            id /= 10;
+        }
+        s[0] = s[0].ToString().ToUpper()[0];
+        return s.ToString();
+    }
+    static TextReader reader;
+    static TextWriter writer;
+    static bool multiTestCases = false;
+    static void Main()
+    {
+        reader = new StreamReader(Console.OpenStandardInput());
+        writer = new StreamWriter(Console.OpenStandardOutput());
+        int n = 1;
+        if (multiTestCases)
+            n = ReadInt();
+        while (n-- > 0)
+            Solve();
+        reader.Close();
+        writer.Close();
+    }
+
+    private static List<int> ReadLeetCodeArray()
+    {
+        string s = reader.ReadLine();
+        List<int> arr = new List<int>();
+        s = s.Substring(1);
+        s = s.Remove(s.Length - 1);
+        var x = s.Split(',');
+        foreach (var m in x)
+            arr.Add(Int32.Parse(m));
+        return arr;
+    }
+    private static List<List<int>> ReadLeetCodeArrayOfArrays()
+    {
+        string s = reader.ReadLine();
+        List<List<int>> arr = new List<List<int>>();
+        s = s.Substring(2);
+        s = s.Remove(s.Length - 2);
+        var arraysString = s.Split("],[");
+        foreach (var numsString in arraysString)
+        {
+            var nums = numsString.Split(',');
+            arr.Add(new List<int>());
+            foreach (var num in nums)
+                arr[arr.Count - 1].Add(Int32.Parse(num));
+        }
+        return arr;
+    }
+
+    private static Queue<string> currentLineTokens = new Queue<string>();
+    private static string[] ReadAndSplitLine() { return reader.ReadLine().Split(new[] { ' ', '\t', }, StringSplitOptions.RemoveEmptyEntries); }
+    public static string ReadToken() { while (currentLineTokens.Count == 0) currentLineTokens = new Queue<string>(ReadAndSplitLine()); return currentLineTokens.Dequeue(); }
+    public static int ReadInt() { return int.Parse(ReadToken()); }
+    public static long ReadLong() { return long.Parse(ReadToken()); }
+    public static double ReadDouble() { return double.Parse(ReadToken(), CultureInfo.InvariantCulture); }
+
+    public static string[] ReadLines(int quantity) { string[] lines = new string[quantity]; for (int i = 0; i < quantity; i++) lines[i] = reader.ReadLine().Trim(); return lines; }
+    public static int[] ReadIntArray() { return ReadAndSplitLine().Select(int.Parse).ToArray(); }
+
+}
+public class Solution
+{
+    public IList<IList<int>> Subsets(int[] nums)
+    {
+        int n = (int)Math.Pow(nums.Length, 2);
+        var ans = new List<IList<int>>();
+        for (int i = 0; i < n; i++)
+        {
+            ans.Add(new List<int>());
+            for (int index = 0; index < nums.Length; index++)
+            {
+                if (((i >> index) & 1) > 0)
+                {
+                    ans[ans.Count - 1].Add(nums[index]);
+                }
+            }
+        }
+        return ans;
+    }
+
+}
+
+public class dsu
+{
+    private List<int> item_id = new List<int>();
+    private List<int> compnent_size = new List<int>();
+    private int size;
+    public int Size
+    {
+        get
+        {
+            return size;
+        }
+    }
+    public dsu(int size)
+    {
+        for (int i = 0; i < size; i++)
+        {
+            item_id.Add(i);
+            compnent_size.Add(1);
+        }
+        this.size = size;
+    }
+    public int find(int id)
+    {
+        int root = id;
+        while (root != item_id[root])
+        {
+            root = item_id[root];
+        }
+        //path comperssion
+        while (root != id)
+        {
+            int next = item_id[id];
+            item_id[id] = root;
+            id = next;
+        }
+        return root;
+    }
+    public void unify(int id1, int id2)
+    {
+        int root1 = find(id1);
+        int root2 = find(id2);
+
+        if (root1 == root2)
+            return;
+        //Union by size
+        if (compnent_size[root1] > compnent_size[root2])
+        {
+            compnent_size[root1] += compnent_size[root2];
+            item_id[root2] = root1;
+        }
+        else
+        {
+            compnent_size[root2] += compnent_size[root1];
+            item_id[root1] = root2;
+        }
+        size--;
+    }
+}
